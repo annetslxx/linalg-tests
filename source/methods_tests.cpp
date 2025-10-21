1a8f32048c63cd69549a9481af79909aa4aa14d5
annetslx <ardolgopolova@edu.hse.ru>
2025-10-21 20:15:32 +0300

    Added LU-decompose method tests

--- source/methods_tests.cpp
+++ source/methods_tests.cpp
@@ -3,6 +3,7 @@
 //
 
 #include <gtest/gtest.h>
+#include <iomanip>
 #include <matrix.h>
 
 using namespace linalg;
@@ -93,4 +94,33 @@ TEST(MatrixTest, SwapMatrices) {
   EXPECT_EQ(m2.rows(), 2);
   EXPECT_EQ(m2.columns(), 2);
   EXPECT_EQ(m2.begin()[0], 1);
+}
+
+TEST(MatrixTest, LUTest) {
+  Matrix A = {{2, 3, 1}, {4, 7, 2}, {6, 18, 5}};
+  std::pair<Matrix, Matrix> pair = A.lu_decompose();
+  EXPECT_TRUE(A == pair.first * pair.second);
+}
+
+TEST(MatrixTest, LUTest2) {
+  Matrix A = {{2, 1, 1}, {4, 3, 3}, {8, 7, 9}};
+  std::pair<Matrix, Matrix> pair = A.lu_decompose();
+  EXPECT_TRUE(A == pair.first * pair.second);
+}
+
+TEST(MatrixTest, SingularMatricesLU) {
+  Matrix zero_pivot = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}};
+
+  Matrix becomes_singular = {{1, 2, 3}, {2, 4, 6}, {7, 8, 9}};
+  EXPECT_THROW(zero_pivot.lu_decompose(), std::runtime_error);
+  EXPECT_THROW(becomes_singular.lu_decompose(), std::runtime_error);
+}
+
+TEST(MatrixTest, NotSquareMatrixLU) {
+  Matrix not_square = {{1, 2, 3}, {4, 5, 6}};
+  EXPECT_THROW(not_square.lu_decompose(), std::runtime_error);
+}
+TEST(MatrixTest, EmpltyMatrixLU) {
+  Matrix empty_matrix(3, 3);
+  EXPECT_THROW(empty_matrix.lu_decompose(), std::runtime_error);
 }
